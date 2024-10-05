ebaa1fb840c784a508f3107cb392e809a34a3fb7
Dorien <33205017+NoLifeDorien@users.noreply.github.com>
2024-10-05 18:10:57 -0400

    Added Target Support

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,6 @@
+Core = nil
+Target = nil
+
 if Config.Framework == "qb" then
     Core = exports['qb-core']:GetCoreObject()
 elseif Config.Framework == "esx" then
@@ -6,6 +9,12 @@ elseif Config.Framework == "nd" then
     Core = exports['nd-core']:getSharedObject()
 elseif Config.Framework == "qbox" then
     Core = exports['qbox-core']:getCoreObject()
+end
+
+if Config.Target == 'ox' then
+    Target = 'ox_target'
+elseif Config.Target == 'qb' then
+    Target = 'qb-target'
 end
 
 exports("GetPlayerJob", function()
@@ -25,3 +34,59 @@ exports("GetPlayerCitizenID", function(text, duration)
     end
     return nil
 end)
+
+exports("AddSphereZone", function(options)
+    local zoneid = nil
+    if TargetSystem == 'ox_target' then
+        zoneid = exports.ox_target:addSphereZone({
+            name = options.name,
+            coords = options.coords,
+            radius = options.radius,
+            options = options.options,
+            debug = options.debug 
+        })
+    elseif TargetSystem == 'qb-target' then
+        zoneid = exports['qb-target']:AddCircleZone(options.name, options.center, options.radius, {
+            name = options.name,
+            debugPoly = options.debug,  
+            useZ = true
+        }, {
+            options = options.options,
+            distance = options.radius
+        })
+    end
+
+    return zoneid
+end)
+
+
+exports("AddTargetEntity", function(entity, options, distance)
+    if TargetSystem == 'ox_target' then
+        exports.ox_target:addLocalEntity(entity, options)
+    elseif TargetSystem == 'qb-target' then
+        exports['qb-target']:AddTargetEntity(entity, {
+            options = options,
+            distance = distance or 5
+        })
+    end
+end)
+
+exports("RemoveTargetEntity", function(entity, options)
+    if TargetSystem == 'ox_target' then
+        exports.ox_target:removeEntity(entity, options)
+    elseif TargetSystem == 'qb-target' then
+        exports['qb-target']:RemoveTargetEntity(entity)
+    end
+end)
+
+exports("RemoveZone", function(zone)
+    if TargetSystem == 'ox_target' then
+        exports.ox_target:removeZone(zone)
+    elseif TargetSystem == 'qb-target' then
+        exports['qb-target']:RemoveZone(zone)
+    end
+end)
+
+
+
+
